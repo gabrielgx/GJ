d9ba036fdd9160b16666854f5a63a02b7e1ab345
Marcos Gabriel Silva Mendes <marcosgabrielgx@gmail.com>
2020-07-23 00:49:41 -0300

    Parte da pasta Assets

---
+++ Assets/Chest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public Transform player;
+    public Animator anim;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(player.position, this.transform.position) < 30)
+        {
+            Vector3 direction = player.position - this.transform.position;
+            float angle = Vector3.Angle(direction, this.transform.forward);
+            //if (Vector3.Distance(player.position, this.transform.position) < 10 && angle < 30)
+            //{
+
+
+                direction.y = 0;
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+                anim.SetBool("isIdle", false);
+                if (direction.magnitude > 5)
+                {
+                    this.transform.Translate(0, 0, 0.05f);
+                    anim.SetBool("isWalking", true);
+                    anim.SetBool("isAttacking", false);
+                }
+                else
+                {
+                    anim.SetBool("isWalking", false);
+                    anim.SetBool("isAttacking", true);
+                }
+            }
+            else
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isAttacking", false);
+                anim.SetBool("isIdle", true);
+            }
+        }
+
+    //}
+}
---
+++ Assets/TimeChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gaia;
+using UnityEngine.UIElements;
+
+public class TimeChange : MonoBehaviour
+{
+    public GaiaGlobal gaia ;
+    //public GaiaLightingProfiles gaiaLightingProfile;  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            gaia.m_lightingProfile.m_lightingProfiles = gaiaLightingProfile;
+        }
+    }*/
+}
