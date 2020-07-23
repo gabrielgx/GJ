949b3ae3d0917e3eea09d08d776a30885be10ace
Marcos Gabriel Silva Mendes <marcosgabrielgx@gmail.com>
2020-07-23 00:54:23 -0300

    Parte da pasta Assets

---
+++ Assets/Resources/Scripts/Game/UI/AbilityUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class AbilityUI : MonoBehaviour
+{
+    [SerializeField] private Image icon = default;
+    [SerializeField] private Image coolDownImage = default;
+
+    public void SetIcon(Sprite s)
+    {
+        icon.sprite = s;
+    }
+    public void ShowCoolDown(float cooldown)
+    {
+        transform.DOComplete();
+        coolDownImage.fillAmount = 0;
+        coolDownImage.DOFillAmount(1, cooldown).SetEase(Ease.Linear).OnComplete(() => transform.DOPunchScale(Vector3.one/10, .2f, 10, 1));
+    }
+}
---
+++ Assets/Resources/Scripts/Game/UI/InterfaceManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamekit3D;
+using UnityEngine.UI;
+
+public class InterfaceManager : MonoBehaviour
+{
+    public CanvasGroup retical;
+
+    [Header("Ability References")]
+    public AbilityUI abilityIconPrefab;
+    public Transform abilityIconSlot;
+
+    void Start()
+    {
+        PlayerController player = PlayerController.instance;
+        for (int i = 0; i < player.m_Abilities.Length; i++)
+        {
+            AbilityUI abilityUi = Instantiate(abilityIconPrefab, abilityIconSlot);
+            player.m_Abilities[i].OnAbilityUse.AddListener((cooldown) => abilityUi.ShowCoolDown(cooldown));
+            abilityUi.SetIcon(player.m_Abilities[i].icon);
+        }
+    }
+
+    void Update()
+    {
+        retical.alpha = PlayerController.instance.m_IsAiming ? 1 : 0;
+    }
+}
